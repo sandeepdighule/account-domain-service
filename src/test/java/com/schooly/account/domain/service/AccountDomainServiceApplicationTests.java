ae2c3fd8d31ad48edc0cccc0f6d28d9ddd342ca6
sandeep <sandeepdighule@gmail.com>
2023-04-16 18:37:43 +0530

    initial commit

---
+++ src/test/java/com/schooly/account/domain/service/AccountDomainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schooly.account.domain.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountDomainServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
